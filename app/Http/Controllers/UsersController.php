063c8d052ee97be88b8f4be4170b63bdcc11e830
Xiangyu <cxy646460179@gamil.com>
2018-05-03 06:12:45 +0000

    Finish 用户注册

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -9,5 +10,29 @@ class UsersController extends Controller
     //用户注册
     public function create(){
         return view('users.create');
+    }
+//    查询用户表显示内容 compact() 把 user 数组的形式传递给 'users.show'
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+//   接收注册信息,进行验证 unique:users 唯一性验证
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+//      用户模型 User::create() 创建成功后会返回一个用户对象，并包含新注册用户的所有信息。
+//      我们将新注册用户的所有信息赋值给变量 $user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+//          当我们想存入一条缓存的数据，让它只在下一次的请求内有效时，则可以使用 flash 方法。
+//          flash 方法接收两个参数，第一个为会话的键，第二个为会话的值
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+//        通过路由跳转来进行数据绑定
+        return redirect()->route('users.show',[$user]);
     }
 }
---
+++ resources/views/shared/_messages.blade.php
@@ -0,0 +1,10 @@
+{{--消息提醒视图，让会话消息在视图上进行展示--}}
+@foreach (['danger', 'warning', 'success', 'info'] as $msg)
+    @if(session()->has($msg))
+        <div class="flash-message">
+            <p class="alert alert-{{ $msg }}">
+                {{ session()->get($msg) }}
+            </p>
+        </div>
+    @endif
+@endforeach
